ea7a99e7395a991b8b4547504bd2385153b7cc52
HaoDongZhang <haodong.zhang@lexisnexis.com>
2020-09-17 13:41:07 +0800

    Run action background

--- src/main/java/com/rzhang/file/upload/plugin/action/PushAction.java
+++ src/main/java/com/rzhang/file/upload/plugin/action/PushAction.java
@@ -3,6 +3,7 @@ package com.rzhang.file.upload.plugin.action;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.rzhang.file.upload.plugin.net.FileUploader;
@@ -17,10 +18,14 @@ public class PushAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
         try {
-            VirtualFile virtualFile = e.getData(PlatformDataKeys.VIRTUAL_FILE);
+            VirtualFile[] virtualFiles = e.getData(PlatformDataKeys.VIRTUAL_FILE_ARRAY);
             Project project = e.getProject();
             assert project != null;
-            FileUploader.getInstance(project).uploadFile(Objects.requireNonNull(virtualFile));
+            assert virtualFiles != null;
+            for (VirtualFile file:virtualFiles) {
+                ApplicationManager.getApplication().executeOnPooledThread(() -> ApplicationManager.getApplication().runReadAction(()
+                        -> FileUploader.getInstance(project).uploadFile(Objects.requireNonNull(file))));
+            }
         } catch (Exception exception) {
             MessageUtils.showAllWarnMsg("error", String.format("Uploaded to server failed, caused by %s", exception.getMessage()));
         }
